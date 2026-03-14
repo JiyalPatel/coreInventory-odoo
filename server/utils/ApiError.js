a935cc059df712741db9bcccac70c1e1bf0f5e0d
JiyalPatel <jiyalpatel10@gmail.com>
2026-03-14 14:35:28 +0530

    Add database connection and error handling; remove unused package.json and stockHelpers.js

---
+++ server/utils/ApiError.js
@@ -0,0 +1,22 @@
+const ApiError = require('../utils/ApiError'); 
+
+const errorMiddleware = (err, req, res, next) => {
+    let { statusCode, message } = err;
+
+    if (!(err instanceof ApiError)) {
+        statusCode = err.statusCode || 500;
+        message = err.message || "Internal Server Error";
+    }
+
+    const response = {
+        success: false,
+        statusCode,
+        message,
+        errors: err.errors || [],
+        ...(process.env.NODE_ENV === "development" && { stack: err.stack })
+    };
+
+    res.status(statusCode).json(response);
+};
+
+module.exports = errorMiddleware;
